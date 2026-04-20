47ce29669638554f47bdc8858af8ce3a7450a0c0
pilotdt <101996172+pilotdt@users.noreply.github.com>
2026-04-20 10:15:57 +0100

    Comment out various script calls in experiments_2.sh
    Comment out additional Python script executions for different configurations.

--- experiments_2.sh
+++ experiments_2.sh
@@ -6,3 +6,21 @@ python3 src/main_mtgnn.py adj_np="data/undirected-adj-mat-all-stations.csv"   bi
 
 python3 src/main_dcrnn.py adj_np="data/binary-adj-mat-all-stations.csv"   binary_adj=True  reg_4_loss=L_dir add_storage=True horizon=7 history=7
 python3 src/main_mtgnn.py adj_np="data/binary-adj-mat-all-stations.csv"   binary_adj=True  reg_4_loss=L_dir add_storage=True horizon=7 history=7
+
+# python3 src/main_dcrnn.py  reg_4_loss=L_dir add_storage=True horizon=10 history=10
+# python3 src/main_mtgnn.py  reg_4_loss=L_dir add_storage=True horizon=10 history=10
+
+# python3 src/main_dcrnn.py adj_np="data/undirected-adj-mat-all-stations.csv"   binary_adj=True  un_adj=True reg_4_loss=L_dir add_storage=True horizon=10 history=10
+# python3 src/main_mtgnn.py adj_np="data/undirected-adj-mat-all-stations.csv"   binary_adj=True  un_adj=True reg_4_loss=L_dir add_storage=True horizon=10 history=10
+
+# python3 src/main_dcrnn.py adj_np="data/binary-adj-mat-all-stations.csv"   binary_adj=True  reg_4_loss=L_dir add_storage=True horizon=10 history=10
+# python3 src/main_mtgnn.py adj_np="data/binary-adj-mat-all-stations.csv"   binary_adj=True  reg_4_loss=L_dir add_storage=True horizon=10 history=10
+
+# python3 src/main_dcrnn.py  reg_4_loss=L_dir add_storage=True horizon=12 history=12
+# python3 src/main_mtgnn.py  reg_4_loss=L_dir add_storage=True horizon=12 history=12
+
+# python3 src/main_dcrnn.py adj_np="data/undirected-adj-mat-all-stations.csv"   binary_adj=True  un_adj=True reg_4_loss=L_dir add_storage=True horizon=12 history=12
+# python3 src/main_mtgnn.py adj_np="data/undirected-adj-mat-all-stations.csv"   binary_adj=True  un_adj=True reg_4_loss=L_dir add_storage=True horizon=12 history=12
+
+# python3 src/main_dcrnn.py adj_np="data/binary-adj-mat-all-stations.csv"   binary_adj=True  reg_4_loss=L_dir add_storage=True horizon=12 history=12
+# python3 src/main_mtgnn.py adj_np="data/binary-adj-mat-all-stations.csv"   binary_adj=True  reg_4_loss=L_dir add_storage=True horizon=12 history=12
